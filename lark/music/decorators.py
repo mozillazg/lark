93d16237cc5cadedcf670523af0f426c9e890c23
Mozillazg <opensource.mozillazg@gmail.com>
2014-07-11 10:46:05 +0800

    精简 json_view 装饰器

--- lark/music/decorators.py
+++ lark/music/decorators.py
@@ -6,42 +6,32 @@ import json
 from django.http import HttpResponse
 
 
-def json_view(func=None, **response_kwargs):
+def json_view(func):
     """装饰 JSON view ::
 
-        @json_view()
+        @json_view
         def view(request):
             return {'data': [1, 2]}
 
-        @json_view(content_type='application/vnd.github+json')
-        def view(request):
-            return {'data': [1, 2]}
-
-        @json_view()
+        @json_view
         def view(request):
             return {'msg': '403'}, 403
     """
-    response_kwargs.setdefault('content_type', 'application/json')
-    response_kwargs.setdefault('status', 200)
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        status = 200
+        content_type = 'application/json'
+        response = func(request, *args, **kwargs)
+        if isinstance(response, HttpResponse):
+            return response
 
-    def decorator(func):
-        @wraps(func)
-        def wrapper(request, *args, **kwargs):
-            response = func(request, *args, **kwargs)
-            if isinstance(response, HttpResponse):
-                return response
+        if isinstance(response, (tuple, list)):
+            status = response[1]
+            context = response[0]
+        else:
+            context = response
 
-            if isinstance(response, (tuple, list)):
-                response_kwargs['status'] = response[1]
-                context = response[0]
-            else:
-                context = response
-
-            content = json.dumps(context)
-            return HttpResponse(content, **response_kwargs)
-        return wrapper
-
-    if func is not None:
-        return decorator(func)
-    else:
-        return decorator
+        content = json.dumps(context)
+        return HttpResponse(content, status=status,
+                            content_type=content_type)
+    return wrapper
