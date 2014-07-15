bd32fcb1d5f47cdbcb8663162a550d55cb793053
Mozillazg <opensource.mozillazg@gmail.com>
2014-07-15 13:45:08 +0800

    调整字段后台显示信息

--- lark/music/models.py
+++ lark/music/models.py
@@ -14,9 +14,9 @@ class Music(TimeStampedModel):
     title = models.CharField(_('title'), max_length=100)
     author = models.CharField(_('author'), max_length=50)
     cover = models.URLField(_('album cover'))
-    douban = models.URLField(_('douban page'), blank=True)
     mp3 = models.URLField(_('mp3 file url'))
     ogg = models.URLField(_('ogg file url'))
+    douban = models.URLField(_('douban page'), blank=True)
     sid = models.CharField(_('SID'), max_length=10, blank=True)
 
     class Meta:
