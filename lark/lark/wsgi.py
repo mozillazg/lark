241ca3dc4962ed67984972668d642ed5e92c6058
Mozillazg <opensource.mozillazg@gmail.com>
2014-07-18 09:24:56 +0800

    前端播放器调整

--- lark/lark/wsgi.py
+++ lark/lark/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, BASE_DIR)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lark.settings")
 
 from django.core.wsgi import get_wsgi_application
